ebeb62f0c63f6b9cb33d759b5971a2c7a0a74cfe
Whitney Green <81326194+whitneygreen@users.noreply.github.com>
2021-06-02 11:06:31 -0400

    Update polybius.js

--- src/polybius.js
+++ src/polybius.js
@@ -1,50 +1,82 @@
-// Please refrain from tampering with the setup code provided here,
-// as the index.html and test files rely on this setup to work properly.
-// Only add code (e.g., helper methods, variables, etc.) within the scope
-// of the anonymous function on line 6
-
-
- // When encoding, it translates the letters i and j to 42.
-//When decoding, it translates 42 to (i/j).
-//It ignores capital letters. (For example, the results of A Message and a message should be the same.)
-//It maintains spaces in the message, before and after encoding or decoding.
-
-
-
 const polybiusModule = (function () {
-  // you can add any code you want within this function scope
 
-  function polybius(input, encode = true) {
-    // your solution code here
 
-   
-
-const grid =
-{
-  '11': 'a', '21': 'b', '31': 'c', '41': 'd', '51': 'e',
-  '12': 'f', '22': 'g', '32': 'h', '42': ['i', 'j'], '52': 'k',
-  '13': 'l', '23': 'm', '33': 'n', '43': 'o', '53': 'p',
-  '14': 'q', '24': 'r', '34': 's', '44': 't', '54': "u",
-  '15': 'v', '25': 'w', '35': 'x', '45': 'y', '55': 'z'
+function polybius(input, encode = true) {
+  //create table for polybius square
+  const conversionTable = [
+    { letter: "a", number: "11" },
+    { letter: "f", number: "12" },
+    { letter: "l", number: "13" },
+    { letter: "q", number: "14" },
+    { letter: "v", number: "15" },
+    { letter: "b", number: "21" },
+    { letter: "g", number: "22" },
+    { letter: "m", number: "23" },
+    { letter: "r", number: "24" },
+    { letter: "w", number: "25" },
+    { letter: "c", number: "31" },
+    { letter: "h", number: "32" },
+    { letter: "n", number: "33" },
+    { letter: "s", number: "34" },
+    { letter: "x", number: "35" },
+    { letter: "d", number: "41" },
+    { letter: "(i/j)", number: "42" },
+    { letter: "o", number: "43" },
+    { letter: "t", number: "44" },
+    { letter: "y", number: "45" },
+    { letter: "e", number: "51" },
+    { letter: "k", number: "52" },
+    { letter: "p", number: "53" },
+    { letter: "u", number: "54" },
+    { letter: "z", number: "55" },
+  ];
+  //floor input
+  input = input.toLowerCase();
+  //create holding variables
+  let temp = "";
+  const result = [];
+  //check if encoding or decoding
+  if (encode) {
+    for (let i = 0; i < input.length; i++) {
+      if (input[i] >= "a" && input[i] <= "z") {
+        temp = conversionTable.find((index) => index.letter.includes(input[i]));
+        temp = temp.number;
+        result.push(temp);
+      } else {
+        result.push(input[i]);
+      }
+    }
+  } else {
+    //check if length of string (without spaces) is even - if not, exit function
+    for (let i = 0; i < input.length; i++) {
+      if (!(input[i] == " ")) {
+        temp += input[i];
+      }
+    }
+    if (temp.length % 2) {
+      return false;
+    }
+    for (let i = 0; i < input.length; ) {
+      //check if current char is a space - if not, decode (current index + next index) & increment i by 2
+      if (!(input[i] == " ")) {
+        const currNum = input[i] + input[i + 1];
+        temp = conversionTable.find((index) => index.number.includes(currNum));
+        temp = temp.letter;
+        i += 2;
+      }
+      //if current char is a space, increment i by 1
+      else {
+        temp = input[i];
+        i++;
+      }
+      result.push(temp);
+    }
+  }
+  return result.join("");
 }
-const gridValues = Object.values(grid)
-const gridKeys = Object.keys(grid)
-
-input = input.toLowerCase();
-
-
-
-
-
-
-
-
-
-
-
   return {
     polybius,
   };
-}});
+})();
 
-module.exports = { polybius: polybiusModule.polybius };
+module.exports = polybiusModule.polybius;
