3b90c925f9d2da50f60ee632f1ebe3a4d553a8b7
Whitney Green <81326194+whitneygreen@users.noreply.github.com>
2021-06-02 15:52:55 -0400

    Update caesar.js

--- src/caesar.js
+++ src/caesar.js
@@ -1,43 +1,31 @@
-// Please refrain from tampering with the setup code provided here,
-// as the index.html and test files rely on this setup to work properly.
-// Only add code (e.g., helper methods, variables, etc.) within the scope
-// of the anonymous function on line 6
-
-
+const caesarModule = (function () {
   // you can add any code you want within this function scope
-  
 
-//input refers to the word/words that are input into the cipher to be encoded/decoded
-//shift refers to a number that will change the input 
-//encode is set to true. if encode is false, we will decode the input instead of encode it
-
- const caesarModule = (function () {
-  
   function caesar(input, shift, encode = true) {
-    if (!shift || shift === 0 || shift < -25 || shift > 25) return false;
-  
-    const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-    input = input.toLowerCase().split("");
-    let result = "";
-  
-    let cipher = (input, shift, alphabet) => {
-      let charSwitch = input.forEach((letter) => {
-        if (letter === " ") {
-          result += letter;
-          return result;
-        }
-        let currentIndex = alphabet.indexOf(letter);
-        let newIndex = currentIndex + shift;
-        if (newIndex > 25) newIndex = newIndex - 26;
-        if (newIndex < 0) newIndex = newIndex + 26;
-  
-        result += alphabet[newIndex];
-      });
-      return result;
-    };
-  
-    return cipher(input, shift, alphabet);
+    // your solution code here
+    if(!shift || shift < -25 || shift > 25 || shift === 0) return false;
+    if(!encode) shift *= -1
+    // change input .toLowerCase, split letters, use charCode (ascii table) to convert letters to numbers,
+    // parseInt on numbers if need to if not right datatype
+    // loop from a to z and z to a
+    return input.toLowerCase().split('').map((letter) => {
+      if (letter.charCodeAt(0) < 97 || letter.charCodeAt(0) > 122) return letter;
+      let num = letter.charCodeAt(0) + parseInt(shift);
+     // loop alphabet if at end or beginning depending on encode/decode
+      if (num >= 122) {
+        num = 96 + (num - 122)
+
+      } if (num <= 96) {
+         num = (num + 122) -96
+      }
+
+      return String.fromCharCode(num);
+    }).join('');
+  }
+
+  return {
+    caesar,
   };
-});
-  
-  module.exports = caesarModule.caesar;
+})();
+
+module.exports = { caesar: caesarModule.caesar };
