6521e05f51b2d3cefb19047cb09e5116389de99c
Whitney Green <81326194+whitneygreen@users.noreply.github.com>
2021-06-02 14:21:53 -0400

    Update substitution.js

--- src/substitution.js
+++ src/substitution.js
@@ -1,6 +1,13 @@
+// Please refrain from tampering with the setup code provided here,
+// as the index.html and test files rely on this setup to work properly.
+// Only add code (e.g., helper methods, variables, etc.) within the scope
+// of the anonymous function on line 6
+
 const substitutionModule = (function () {
   // you can add any code you want within this function scope
 
+
+  
   function substitution(input, alphabet, encode = true) {
     // your solution code here
 
@@ -87,4 +94,4 @@ const substitutionModule = (function () {
   };
 })();
 
-module.exports = substitutionModule.substitution;
+module.exports = { substitution: substitutionModule.substitution };
