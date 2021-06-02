b8b17f7c9701109b93bdca331e3ffc367122171e
Whitney Green <81326194+whitneygreen@users.noreply.github.com>
2021-06-02 14:22:24 -0400

    Update polybius.js

--- src/polybius.js
+++ src/polybius.js
@@ -1,7 +1,15 @@
-const polybiusModule = (function () {
+// Please refrain from tampering with the setup code provided here,
+// as the index.html and test files rely on this setup to work properly.
+// Only add code (e.g., helper methods, variables, etc.) within the scope
+// of the anonymous function on line 6
 
 
-function polybius(input, encode = true) {
+const polybiusModule= (function () {
+  // you can add any code you want within this function scope
+
+  function polybius(input, encode = true) {
+
+
   //create table for polybius square
   const conversionTable = [
     { letter: "a", number: "11" },
@@ -79,4 +87,4 @@ function polybius(input, encode = true) {
   };
 })();
 
-module.exports = polybiusModule.polybius;
+module.exports = { polybius: polybiusModule.polybius };
